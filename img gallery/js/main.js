accc6a595f2bba51211e336aa41639eca1e0ad08
Hriday Purohit <hridaypurohit@gmail.com>
2023-11-28 17:18:54 -0500

    Trial stuff

--- img gallery/js/main.js
+++ img gallery/js/main.js
@@ -1,10 +1,19 @@
-
-
- document.addEventListener("click",function (e){
-   if(e.target.classList.contains("gallery-item")){
-   	  const src = e.target.getAttribute("src");
-   	  document.querySelector(".modal-img").src = src;
-   	  const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
-   	  myModal.show();
-   }
- })
+// Add click event listener to each gallery item  
+const galleryItems = document.querySelectorAll(".gallery-item");  
+galleryItems.forEach(item => {  
+  item.addEventListener("click", function () {  
+    const src = item.querySelector("img").getAttribute("src");  
+    document.querySelector(".modal-img").src = src;  
+    const myModal = new bootstrap.Modal(document.getElementById("gallery-modal"), {  
+      backdrop: 'static',  
+      keyboard: false  
+    });  
+    myModal.show();  
+  });  
+});  
+  
+// Randomize photo angles  
+galleryItems.forEach(item => {  
+  const randomAngle = Math.floor(Math.random() * 41) - 20; // Random angle between -20 and 20 degrees  
+  item.style.transform = `rotateZ(${randomAngle}deg)`;  
+});  
